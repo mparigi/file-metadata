3c562db2d402037c814583784ea170d1841862bb
Makarand <mparigi99@gmail.com>
2016-07-29 19:11:20 +0000

    fixed 'get' path

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", function (req, res) {
-    res.sendFile("index.html");
+    res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(process.env.PORT || 8080);
